f8301b441cbb2c619f9943e45ea5cb62861c5267
TiagoUniverse <tiagocesar68@gmail.com>
2023-02-26 00:44:23 -0300

    Criando função de soma

--- index.php
+++ index.php
@@ -17,4 +17,28 @@ $pessoa->setNome("Tiagão");
 
 echo "<br>" . $pessoa->getNome();
 
+
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu PHP</title>
+</head>
+<body>
+    
+
+<br><br>
+<form action="index.php" method="GET">
+    <input type="number" name="numero1" >
+    <input type="number" name="numero2" >
+    <button type="submit">Somar</button>
+</form>
+
+</body>
+</html>
