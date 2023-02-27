083b148f5f40c4bfb1a0475d852590b3f4a424f8
TiagoUniverse <tiagocesar68@gmail.com>
2023-02-27 13:54:27 -0300

    Parte de soma ajustada

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
  * Data: 25/02/23
  */
 
- require_once "model/pessoa.php";
+require_once "model/pessoa.php";
+
 use model\pessoa;
 
 echo "Hello world";
@@ -20,24 +21,24 @@ echo "<br>" . $pessoa->getNome();
 
 
 // Soma
-if (!isset($_POST['numero1'])){
+if (!isset($_POST['numero1'])) {
     $_POST['numero1'] = 0;
 }
 
 
-if (!isset($_POST['numero2'])){
+if (!isset($_POST['numero2'])) {
     $_POST['numero2'] = 0;
 }
 
-if (!isset($_POST['iniciar'])){
+if (!isset($_POST['iniciar'])) {
     $_POST['iniciar'] = "NÃO INICIADO";
 }
 
-if (isset($_POST['numero1']) && isset($_POST['numero2'])  && isset($_POST['iniciar']) && $_POST['iniciar'] == "SOMAR" ){
-//    echo $_POST['numero1']; 
-//    echo "<br>" . $_POST['numero2'];
+if (isset($_POST['numero1']) && isset($_POST['numero2'])  && isset($_POST['iniciar']) && $_POST['iniciar'] == "SOMAR") {
+    //    echo $_POST['numero1']; 
+    //    echo "<br>" . $_POST['numero2'];
 
-   echo "<br>" . ($_POST['numero1'] + $_POST['numero2']);
+    echo "<br>" . ($_POST['numero1'] + $_POST['numero2']);
 }
 
 
@@ -45,22 +46,25 @@ if (isset($_POST['numero1']) && isset($_POST['numero2'])  && isset($_POST['inici
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu PHP</title>
 </head>
-<body>
-    
 
-<br><br>
-<form action="index.php" method="post">
-    <input type="hidden" name="iniciar" value="SOMAR">
-    <input type="number" name="numero1" value="<?php echo $_POST['numero1'] ; ?>" >
-    <input type="number" name="numero2" value="<?php echo $_POST['numero2'] ; ?>" >
-    <button type="submit">Somar</button>
-</form>
+<body>
+
+
+    <br><br>
+    <form action="index.php" method="post">
+        <input type="hidden" name="iniciar" value="SOMAR">
+        <input type="number" name="numero1" value="<?php echo $_POST['numero1']; ?>">
+        <input type="number" name="numero2" value="<?php echo $_POST['numero2']; ?>">
+        <button type="submit">Somar</button>
+    </form>
 
 </body>
+
 </html>
